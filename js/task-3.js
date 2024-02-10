86ac19646b42d9d253d0a2865eddad0c848dbde9
Oleg007git <cusniroleg@icloud.com>
2024-02-09 16:32:25 -0800

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,6 @@
-const sortByDescendingFriendCount = users.toSorted((a, b) => b - a);
+const sortByDescendingFriendCount = (users) => {
+  return users.sort((a, b) => b.friends.length - a.friends.length);
+};
 console.log(
   sortByDescendingFriendCount([
     {
